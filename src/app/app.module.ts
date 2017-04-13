136e1e94ee612a61ca54ce7e7039e3b6192a0cb3
recuritis <recuritis@gmail.com>
2017-04-13 08:56:19 +0800

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './exports/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
